1c64c55260730ca2182ed5bbd98639e4ef56faab
Adam <abobich675@gmail.com>
2025-01-24 18:06:48 -0800

    set up control system

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -1,9 +1,17 @@
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    float speed = 5;
+    const float GRAVITYSCALE = 1;
+
+    float Speed = 5;
+    float FastFallMultiplier = 1.25f;
+
+    public Camera camera;
+
+
 
     Rigidbody2D rb;
 
@@ -23,17 +31,39 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector2 moveInput = moveAction.ReadValue<Vector2>();
-        rb.linearVelocityX = moveInput.x * speed;
+        DoMovement();
 
         if (interactAction.triggered)
         {
-            Debug.Log("Interact");
-            // Vector3 mousePos = Input.mousePosition;
-            // {
-            //     Debug.Log(mousePos.x);
-            //     Debug.Log(mousePos.y);
-            // }
+            Debug.Log("Interact Triggered");
+            {
+                RaycastHit hit;
+                Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+                Debug.DrawRay(ray.origin, ray.direction * 100, Color.yellow, 2);
+
+                if (Physics.Raycast(ray, out hit))
+                {
+                    Debug.Log(hit.transform.name);
+                }
+            }
+        }
+
+        void DoMovement()
+        {
+            // Movement
+            Vector2 moveInput = moveAction.ReadValue<Vector2>();
+            rb.linearVelocityX = moveInput.x * Speed;
+
+            // Fast Fall
+            if (moveInput.y < 0)
+            {
+                Debug.Log("Fast Fall");
+                rb.gravityScale = GRAVITYSCALE * FastFallMultiplier;
+            }
+            else
+            {
+                rb.gravityScale = GRAVITYSCALE;
+            }
         }
     }
 }
