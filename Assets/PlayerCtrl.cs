0313d8286dfb701a9a402e9fad577ccf4c0c4574
lukegarci <lukegarci@gmail.com>
2025-01-26 00:40:32 -0800

    Main Menu/Pre-Lobby
    When the play button is pressed you are able to be taken to a pre lobby

---
+++ Assets/PlayerCtrl.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class PlayerCtrl : MonoBehaviour
+{
+    public float movSpeed;
+    float speedx, speedy;
+    Rigidbody2D rb; // Correct type is Rigidbody2D for 2D physics
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>(); // Correctly get the Rigidbody2D component
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        speedx = Input.GetAxisRaw("Horizontal") * movSpeed;
+        speedy = Input.GetAxisRaw("Vertical") * movSpeed;
+
+        // Use linearVelocity or velocity for Rigidbody2D
+        rb.linearVelocity = new Vector2(speedx, speedy);
+    }
+}
