e1f8a6e7b901ec31839403e8d7d6a3b9de0e31bc
ryandobkin <ryandobkin2@gmail.com>
2025-01-30 14:41:49 -0800

    Created local player controller script and updated coin script
    Updated coin collection animation to make it similar to games like Mario. Created LocalPlayerController to have an easy way to test controlls.

--- Assets/Scripts/TokenInstance.cs
+++ Assets/Scripts/TokenInstance.cs
@@ -15,7 +15,7 @@ public class TokenInstance : MonoBehaviour
     {
         if (col.gameObject.tag == "Player")
         {
-            StartCoroutine(LerpPosition(transform.position, 0.75f, 0.5f));
+            StartCoroutine(LerpPosition(transform.position, 1, 0.5f));
             ScoreManager.instance.AddPoint();
             GetComponent<BoxCollider2D>().enabled = false;
             Debug.Log("ScoreManager - destroyed token" + col.gameObject);
@@ -24,22 +24,24 @@ public class TokenInstance : MonoBehaviour
     
     public static float CustomInterpolation(float t)
     {
-        t = Mathf.Clamp01(t);
+        //t = Mathf.Clamp01(t);
 
-        float sMid = Mathf.Sin(t * Mathf.PI * 1.2f);
-        float fStart = Mathf.Pow(t, 0.15f);
-        float fEnd = Mathf.Pow(1 - t, 0.5f);
-        float overshoot = Mathf.SmoothStep(0, 0.2f, t);
+        // float sMid = Mathf.Sin(t * Mathf.PI * 1.2f);
+        // float fStart = Mathf.Pow(t, 0.15f);
+        // float fEnd = Mathf.Pow(1 - t, 0.5f);
+        // float overshoot = Mathf.SmoothStep(0, 0.2f, t);
 
-        //return fStart * sMid + (1 - fEnd) * 0.15f + overshoot;
-        if (t <= 0.5)
-        {
-            return (-1) * Mathf.Pow(Mathf.Sin(Mathf.PI * t + Mathf.PI / 2), 4) + 1;
-        }
-        else
-        {
-            return (-1) * Mathf.Pow(Mathf.Sin(Mathf.PI * t + Mathf.PI + Mathf.PI / 2), 4) + 1;
-        }
+        // //return fStart * sMid + (1 - fEnd) * 0.15f + overshoot;
+        // if (t <= 0.5)
+        // {
+        //     return (-1) * Mathf.Pow(Mathf.Sin(Mathf.PI * t + Mathf.PI / 2), 4) + 1;
+        // }
+        // else
+        // {
+        //     return (-1) * Mathf.Pow(Mathf.Sin(Mathf.PI * t + Mathf.PI + Mathf.PI / 2), 4) + 1;
+        // }
+
+        return -8*(Mathf.Pow(t-0.35f, 2))/(Mathf.Sqrt(1+Mathf.Pow(t, 2)))+1;
 
     }
 
@@ -53,7 +55,7 @@ public class TokenInstance : MonoBehaviour
         {
             time += Time.deltaTime;
             float easedT = CustomInterpolation(time);
-            transform.position = Vector3.Slerp(startPos, targetPosition, easedT);
+            transform.position = Vector3.Lerp(startPos, targetPosition, easedT);
             yield return null;
         }
         Destroy(gameObject);
