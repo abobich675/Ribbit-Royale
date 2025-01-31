9e2589304ccb4555e1c37a1a5d2e2b827ea22810
lukegarci <lukegarci@gmail.com>
2025-01-31 03:13:30 -0800

    Loading and Game Full messages when clicking join game

--- Assets/Scripts/RibbitRoyaleMultiplayer.cs
+++ Assets/Scripts/RibbitRoyaleMultiplayer.cs
@@ -8,6 +8,9 @@ public class RibbitRoyaleMultiplayer: NetworkBehaviour
     private const int MAX_PLAYER_AMOUNT = 4;
     public static RibbitRoyaleMultiplayer Instance { get; private set;}
 
+    public event EventHandler OnTryingToJoinGame;
+    public event EventHandler OnTFailedToJoinGame;
+
     private void Awake(){
         Instance = this;
         DontDestroyOnLoad(gameObject);
@@ -18,15 +21,23 @@ public class RibbitRoyaleMultiplayer: NetworkBehaviour
         NetworkManager.Singleton.StartHost();
     }
 
+    private void NetworkManager_OnClientDisconnectCallback(ulong clientId)
+    {
+        OnTFailedToJoinGame?.Invoke(this,EventArgs.Empty);
+    }
+
     private void NetworkManager_ConnectionApprovalCallback(NetworkManager.ConnectionApprovalRequest connectionApprovalRequest, NetworkManager.ConnectionApprovalResponse connectionApprovalResponse)
     {
+        if(IsServer){
+            connectionApprovalResponse.Approved = true;
+        }
         if (SceneManager.GetActiveScene().name != Loader.Scene.CharacterSelectScene.ToString()){
             connectionApprovalResponse.Approved = false;
             connectionApprovalResponse.Reason = "Game has already started";
             return;
         }
         if (NetworkManager.Singleton.ConnectedClients.Count >=  MAX_PLAYER_AMOUNT){
-                        connectionApprovalResponse.Approved = false;
+            connectionApprovalResponse.Approved = false;
             connectionApprovalResponse.Reason = "Game is full";
             return;
         }
@@ -34,7 +45,11 @@ public class RibbitRoyaleMultiplayer: NetworkBehaviour
 
     }
 
-    public void StartClient(){
+    public void StartClient()
+    {
+        OnTryingToJoinGame?.Invoke(this,EventArgs.Empty);
+
+        NetworkManager.Singleton.OnClientDisconnectCallback += NetworkManager_OnClientDisconnectCallback;
         NetworkManager.Singleton.StartClient();
 
     }
---
+++ Assets/Scripts/UI/ConnectingResponseMessageUI.cs
@@ -0,0 +1,37 @@
+using System;
+using TMPro;
+using Unity.Netcode;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ConnectingResponseMessageUI : MonoBehaviour
+{
+
+    [SerializeField] private TextMeshProUGUI messageText;
+    [SerializeField] private Button closeButton;
+
+    private void Awake (){
+        closeButton.onClick.AddListener(Hide);
+    }
+    private void Start(){
+        RibbitRoyaleMultiplayer.Instance.OnTFailedToJoinGame += RibbitRoyaleMultiplayer_OnFailedToJoinGame;
+        Hide();
+    }
+
+    private void RibbitRoyaleMultiplayer_OnFailedToJoinGame(object sender, EventArgs e)
+    {
+        Show();
+        messageText.text = NetworkManager.Singleton.DisconnectReason;
+    }
+
+    private void Show(){
+        gameObject.SetActive(true);
+    }
+    private void Hide(){
+        gameObject.SetActive(false);
+    }
+
+    private void OnDestroy(){
+        RibbitRoyaleMultiplayer.Instance.OnTFailedToJoinGame -= RibbitRoyaleMultiplayer_OnFailedToJoinGame;
+    }
+}
