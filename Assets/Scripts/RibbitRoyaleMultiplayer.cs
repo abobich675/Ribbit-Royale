4c0897ac6379e07802be03ea718c776932ce3c5a
lukegarci <lukegarci@gmail.com>
2025-01-31 01:20:53 -0800

    Set the max amount of players to the server to 4

--- Assets/Scripts/RibbitRoyaleMultiplayer.cs
+++ Assets/Scripts/RibbitRoyaleMultiplayer.cs
@@ -1,9 +1,11 @@
 using System;
 using Unity.Netcode;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class RibbitRoyaleMultiplayer: NetworkBehaviour
 {
+    private const int MAX_PLAYER_AMOUNT = 4;
     public static RibbitRoyaleMultiplayer Instance { get; private set;}
 
     private void Awake(){
@@ -18,8 +20,18 @@ public class RibbitRoyaleMultiplayer: NetworkBehaviour
 
     private void NetworkManager_ConnectionApprovalCallback(NetworkManager.ConnectionApprovalRequest connectionApprovalRequest, NetworkManager.ConnectionApprovalResponse connectionApprovalResponse)
     {
+        if (SceneManager.GetActiveScene().name != Loader.Scene.CharacterSelectScene.ToString()){
+            connectionApprovalResponse.Approved = false;
+            connectionApprovalResponse.Reason = "Game has already started";
+            return;
+        }
+        if (NetworkManager.Singleton.ConnectedClients.Count >=  MAX_PLAYER_AMOUNT){
+                        connectionApprovalResponse.Approved = false;
+            connectionApprovalResponse.Reason = "Game is full";
+            return;
+        }
         connectionApprovalResponse.Approved = true;
-        connectionApprovalResponse.CreatePlayerObject = true;
+
     }
 
     public void StartClient(){
