04e36a6eb304f2165e5b58ebc85f2610edf6b3e6
ryandobkin <ryandobkin2@gmail.com>
2025-02-17 21:43:04 -0800

    Worked on scoremanager refactor.
    Created ScoreEntryUI to control UI values, letting ScoreEntry directly control the UI without library calls. Refactored more of ScoreManager to use new ScoreEntry object, no longer reliant on deprecated ver.

--- Assets/Scripts/ScoreEntry.cs
+++ Assets/Scripts/ScoreEntry.cs
@@ -1,12 +1,40 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ScoreEntry
 {
+
     private Color entryColor;
     private int entryRank;
     private int entryScore;
+    private Image entryAvatar;
     private string entryPlayerName;
     private GameObject entryGameObject;
+    private float yPos;
+
+    private ScoreEntryUI uiController;
+
+    public void Initialize()
+    {
+        uiController = entryGameObject.GetComponent<ScoreEntryUI>();
+        entryColor = Color.white;
+        entryRank = 0;
+        entryScore = 0;
+        //entryPlayerName = "default";
+        //entryGameObject = null;
+        yPos = 0;
+    }
+    
+    // get, set ypos
+    public float GetY()
+    {
+        return yPos;
+    }
+
+    public void SetY(float newY)
+    {
+        yPos = newY;
+    }
     
     
     // get, set gameobject
@@ -32,14 +60,26 @@ public class ScoreEntry
     }
 
     // get, set color
-    public Color GetColor()
+    public Color GetEntryColor()
     {
         return entryColor;
     }
 
-    public void SetColor(Color color)
+    public void SetEntryColor(Color color)
     {
         entryColor = color;
+    }
+    
+    // get, set avatar
+    public Image GetAvatar()
+    {
+        return entryAvatar;
+    }
+
+    public void SetAvatar(Image newAvatar)
+    {
+        entryAvatar = newAvatar;
+        uiController.avatarImage = entryAvatar;
     }
     
     // get, set rank
@@ -51,6 +91,7 @@ public class ScoreEntry
     public void SetRank(int rank)
     {
         entryRank = rank;
+        uiController.rankText.text = entryRank.ToString();
     }
     
     // get, set score
@@ -62,5 +103,6 @@ public class ScoreEntry
     public void SetScore(int score)
     {
         entryScore = score;
+        uiController.scoreText.text = entryScore.ToString();
     }
 }
--- Assets/Scripts/UI/ScoreManager.cs
+++ Assets/Scripts/UI/ScoreManager.cs
@@ -1,6 +1,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 using System.Collections;
+using System.Linq;
+using Unity.VisualScripting;
+using UnityEngine.SocialPlatforms.Impl;
 using UnityEngine.UI;
 
 
@@ -12,12 +15,12 @@ public class ScoreManager : MonoBehaviour
     public Transform scoreboardContent;
     public float moveDuration = 10.0f;
 
-    public Sprite spriteRed;
-    public Sprite spriteBlue;
-    public Sprite spriteGreen;
-    public Sprite spriteYellow;
+    public Image spriteRed;
+    public Image spriteBlue;
+    public Image spriteGreen;
+    public Image spriteYellow;
 
-    private Dictionary<string, (Color, Sprite)> colorSpriteDictionary = new Dictionary<string, (Color, Sprite)>();
+    private Dictionary<string, (Color, Image)> colorSpriteDictionary = new Dictionary<string, (Color, Image)>();
     private Dictionary<string, string> playerColorDictionary = new Dictionary<string, string>();
 
     //private Dictionary<string, (int score, GameObject entry)> playerEntries = new Dictionary<string, (int, GameObject)>();
@@ -56,94 +59,107 @@ public class ScoreManager : MonoBehaviour
 
     private ScoreEntry AddPlayer(string playerName)
     {
+        // Creates new ScoreEntry object, sets parameters, adds to scoreEntries list, returns scoreEntry object
         ScoreEntry scoreEntry = new ScoreEntry();
         scoreEntry.SetPlayerName(playerName);
+        scoreEntry.SetGameObject(Instantiate(scoreEntryPrefab, scoreboardContent));
+        scoreEntry.Initialize();
+        scoreEntries.Add(scoreEntry);
         return scoreEntry;
     }
 
     public void UpdatePlayerScore(string playerName, int score)
     {
-        if (playerEntries.Contains(ScoreEntry.))
-        
-        // if already existing profile
-        if (playerEntries.ContainsKey(playerName))
+        foreach (ScoreEntry entry in scoreEntries)
         {
-            playerEntries[playerName] = (score, playerEntries[playerName].entry);
-            UpdateScoreText(playerEntries[playerName].entry, score);
+            if (entry.GetPlayerName() == playerName)
+            {
+                entry.SetScore(score);
+                return;
+            }
         }
-        else
+        
         // if creating new user in leaderboard
         {
-            Debug.Log("Instantiating new profile in scoreboard " + playerName);
-            GameObject newEntry = Instantiate(scoreEntryPrefab, scoreboardContent);
-            UpdateScoreText(newEntry, score);
+            var entry = AddPlayer(playerName);
+            UpdateScoreText(entry, score);
+            
+            // temp takes color+avatar from premade dict
             string playerColor = playerColorDictionary[playerName];
             var tupleColor = colorSpriteDictionary[playerColor];
-            newEntry.transform.Find("AvatarImage").GetComponent<Image>().sprite = tupleColor.Item2;
-            newEntry.transform.Find("AvatarBorder").GetComponent<Image>().color = tupleColor.Item1;
-            playerEntries.Add(playerName, (score, newEntry));
-        }
-    }
-    
-    public void IncrementPlayerScore(string playerName, int amount)
-    {
-        if (playerEntries.ContainsKey(playerName))
-        {
-            int newScore = playerEntries[playerName].score + amount;
-            UpdatePlayerScore(playerName, newScore);
-        }
-        else
-        {
-            Debug.Log("Player not instantiated, IncrementPlayerScore() ERROR");
+            
+            UpdateEntryColors(entry, tupleColor.Item1, tupleColor.Item2);
         }
     }
 
-    private void UpdateScoreText(GameObject entry, int score)
+    public void UpdateEntryColors(ScoreEntry entry, Color teamColor, Image avatar)
+    {
+        entry.SetAvatar(avatar);
+        entry.SetEntryColor(teamColor);
+    }
+
+    private void UpdateScoreText(ScoreEntry entry, int score)
     // Updates the score field of a given player
     // Takes GameObject entry [player entry in scoreboard], playerName [string], score [int]
     {
-        entry.transform.Find("RankText").GetComponent<Text>().text = "?";
-        entry.transform.Find("PointsText").GetComponent<Text>().text = $"{score}";
+        entry.SetScore(score);
+        
+        //entry.transform.Find("RankText").GetComponent<Text>().text = "?";
+        //entry.transform.Find("PointsText").GetComponent<Text>().text = $"{score}";
         UpdateRanking();
     }
 
     private void UpdateRanking()
     {
         // Creates a list <string, (score, entry)> sortedPlayers
-        List<KeyValuePair<string, (int score, GameObject entry)>> sortedPlayers =
-            new List<KeyValuePair<string, (int, GameObject)>>(playerEntries);
+        // List<KeyValuePair<string, (int score, ScoreEntry entry)>> sortedPlayers =
+        //     new List<KeyValuePair<string, (int, ScoreEntry)>>(scoreEntries);
         
+        var sortedP = scoreEntries;
+
         // Sorts sortedPlayers list by (a, b), and (b score CompareTo a score)
-        sortedPlayers.Sort((a, b) => b.Value.score.CompareTo(a.Value.score));
+        
+        //sortedPlayers.Sort((a, b) => b.Value.score.CompareTo(a.Value.score));
+        
         // x.CompareTo(y) will return 0 if sorted (e.g. x == y), -1 if preceding (e.g. x < y), +1 if should follow (e.g. x > y)
         
+        sortedP.Sort((a, b) => b.GetScore().CompareTo(a.GetScore()));
+        
         // Creates an array startPositions of player-entry:y-value
-        Dictionary<GameObject, float> startPositions = new Dictionary<GameObject, float>();
-        foreach (var player in sortedPlayers)
+        // Dictionary<GameObject, float> startPositions = new Dictionary<GameObject, float>();
+        
+        // foreach (var player in sortedPlayers)
+        // {
+        //     startPositions[player.Value.entry] = player.Value.entry.transform.localPosition.y;
+        // }
+
+        foreach (var entry in sortedP)
         {
-            startPositions[player.Value.entry] = player.Value.entry.transform.localPosition.y;
+            entry.SetY(entry.GetGameObject().transform.localPosition.y);
         }
         
         // Sets the siblingindex for each entry in sortedPlayers - reorders object hierarchy in scene 
-        for (int i = 0; i < sortedPlayers.Count; i++)
+        for (int i = 0; i < sortedP.Count; i++)
         {
-            sortedPlayers[i].Value.entry.transform.SetSiblingIndex(i);
+            sortedP[i].GetGameObject().transform.SetSiblingIndex(i);
         }
 
         Debug.Log("Rankings Sorted, Starting AnimateRankChange()");
-        StartCoroutine(AnimateRankChange(sortedPlayers, startPositions));
+        if (sortedP != scoreEntries)
+        {
+            StartCoroutine(AnimateRankChange(sortedP));
+        }
     }
 
-    private IEnumerator AnimateRankChange(List<KeyValuePair<string, (int score, GameObject entry)>> sortedPlayers, Dictionary<GameObject, float> startPositions)
+    private IEnumerator AnimateRankChange(List<ScoreEntry> sortedP)
     {
-        
         yield return null;
         
         Dictionary<GameObject, float> targetPositions = new Dictionary<GameObject, float>();
         // Gets the current y value of each entry sorted by score
-        foreach (var player in sortedPlayers)
+        foreach (var player in sortedP)
         {
-            targetPositions[player.Value.entry] = player.Value.entry.transform.localPosition.y;
+            targetPositions[player.GetGameObject()] = player.GetGameObject().transform.localPosition.y;
         }
         
         float elapsed = 0f;
@@ -159,10 +175,10 @@ public class ScoreManager : MonoBehaviour
             // interpolates all entries, moving 1.y to 2.y and vice versa
             // if no change, moves 1.y to 1.y, etc.
             // needs updating to only interpolate relevant entries, and only call on relevant score update
-            foreach (var player in sortedPlayers)
+            foreach (var player in sortedP)
             {
-                GameObject playerEntry = player.Value.entry;
-                float startY = startPositions[playerEntry];
+                GameObject playerEntry = player.GetGameObject();
+                float startY = player.GetY();
                 float targetY = targetPositions[playerEntry];
 
                 playerEntry.transform.localPosition = new Vector3(
@@ -176,19 +192,18 @@ public class ScoreManager : MonoBehaviour
 
         // After animation is done, iterates through sortedPlayers and assigns their rank to their index + 1
         // Really, each entry should be an object that can be easily updated, rework that next!
-        for (int i = 0; i < sortedPlayers.Count; i++)
+        for (int i = 0; i < sortedP.Count; i++)
         {
-            GameObject playerEntry = sortedPlayers[i].Value.entry;
-            playerEntry.transform.Find("RankText").GetComponent<Text>().text = (i + 1).ToString();
+            sortedP[i].SetRank(i+1);
         }
     }
 
-    public void RemovePlayer(string playerName)
-    {
-        if (playerEntries.ContainsKey(playerName))
-        {
-            Destroy(playerEntries[playerName].entry);
-            playerEntries.Remove(playerName);
-        }
-    }
+//     public void RemovePlayer(string playerName)
+//     {
+//         if (playerEntries.ContainsKey(playerName))
+//         {
+//             Destroy(playerEntries[playerName].entry);
+//             playerEntries.Remove(playerName);
+//         }
+//     }
 }
