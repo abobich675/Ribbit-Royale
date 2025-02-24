faef8ef92488310cd17211e7fd89a8ef60cfd648
Adam <abobich675@gmail.com>
2025-02-23 18:42:01 -0800

    start at basic prelobby

--- Assets/Scripts/PreLobby/PlayerCtrl.cs
+++ Assets/Scripts/PreLobby/PlayerCtrl.cs
@@ -7,36 +7,17 @@ public class PlayerCtrl : NetworkBehaviour
     public float movSpeed;
     private float speedx, speedy;
     private Rigidbody2D rb;
-    private Camera playerCamera;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-
-        // Ensure only the local player has an active camera
-        if (IsOwner)
-        {
-            SetupCamera();
-        }
+        
         // This will get a players data based off the clientId 
         PlayerData playerData = RibbitRoyaleMultiplayer.Instance.GetPlayerDataFromClientId(OwnerClientId);
         // Using the player data call the function SetPlayerColor, get the players color using the playerData
         playerVisual.SetPlayerColor(RibbitRoyaleMultiplayer.Instance.GetPlayerColor(playerData.colorId));
 
 
-    }
-
-    void SetupCamera()
-    {
-        // Create a new camera for this player
-        playerCamera = new GameObject("Player Camera").AddComponent<Camera>();
-
-        // Attach the camera to the player
-        playerCamera.transform.SetParent(transform);
-        playerCamera.transform.localPosition = new Vector3(0, 0, -10); // Offset the camera behind the player
-
-        // Make sure this camera is active and rendering only for this player
-        playerCamera.gameObject.tag = "MainCamera";
     }
 
     void Update()
