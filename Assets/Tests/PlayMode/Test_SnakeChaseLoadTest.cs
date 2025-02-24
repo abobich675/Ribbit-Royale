f4e097ef5220b9cd67a382186a726747c6e7c434
Adam <abobich675@gmail.com>
2025-02-24 14:53:19 -0800

    tongue swing load test

--- Assets/Tests/PlayMode/Test_SnakeChaseLoadTest.cs
+++ Assets/Tests/PlayMode/Test_SnakeChaseLoadTest.cs
@@ -25,4 +25,24 @@ public class SceneLoadTest
         // Set an assertion for reasonable load time
         Assert.Less(loadTime, 5f, "Scene took too long to load!");
     }
+
+    [UnityTest]
+    public IEnumerator TestTongueSwingSceneLoadTime()
+    {
+        float startTime = Time.realtimeSinceStartup;
+        
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync("TongueSwingGame");
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+        }
+
+        yield return new WaitForSeconds(1f);
+
+        float loadTime = Time.realtimeSinceStartup - startTime;
+        Debug.Log($"TongueSwingScene Load Time: {loadTime} seconds");
+
+        // Set an assertion for reasonable load time
+        Assert.Less(loadTime, 5f, "Scene took too long to load!");
+    }
 }
