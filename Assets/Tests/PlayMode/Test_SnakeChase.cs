b0b2eec50a794be8400d0e065f010320b9cd9c1e
Adam <abobich675@gmail.com>
2025-02-24 15:00:55 -0800

    tests for tongueswing and count the animal

---
+++ Assets/Tests/PlayMode/Test_SnakeChase.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.TestTools;
+
+public class SnakeChaseTests
+{
+    float MAX_LOAD_TIME = 5f;
+
+    [UnityTest]
+    public IEnumerator TestSnakeChaseSceneLoadTime()
+    {
+        float startTime = Time.realtimeSinceStartup;
+        
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync("SnakeChaseGame");
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+        }
+
+        yield return new WaitForSeconds(1f);
+
+        float loadTime = Time.realtimeSinceStartup - startTime;
+        Debug.Log($"SnakeChase Scene Load Time: {loadTime} seconds");
+
+        // Set an assertion for reasonable load time
+        Assert.Less(loadTime, MAX_LOAD_TIME, "Scene took too long to load!");
+    }
+}
