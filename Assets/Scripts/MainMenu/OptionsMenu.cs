9f7711217ae6089e9ed5c522b3aeb09df1362dd4
Aidan Caughey <caugheya@oregonstate.edu>
2025-03-09 17:26:12 -0700

    OptionsMenu functionality
    Added volume and fullscreen functionality

--- Assets/Scripts/MainMenu/OptionsMenu.cs
+++ Assets/Scripts/MainMenu/OptionsMenu.cs
@@ -1,6 +1,31 @@
 using UnityEngine;
+using UnityEngine.UI;
 
-public class OptionsMenu
+public class OptionsMenu : MonoBehaviour
 {
-    
+    [SerializeField] private Slider volumeSlider;
+    [SerializeField] private Toggle fullscreenToggle;
+
+    private Resolution[] resolutions;
+
+    private void Start()
+    {
+        // Load saved settings
+        volumeSlider.value = PlayerPrefs.GetFloat("Volume", 1f);
+        fullscreenToggle.isOn = PlayerPrefs.GetInt("Fullscreen", 1) == 1;
+        AudioListener.volume = volumeSlider.value;
+        Screen.fullScreen = fullscreenToggle.isOn;
+    }
+
+    public void SetVolume(float volume)
+    {
+        AudioListener.volume = volume;
+        PlayerPrefs.SetFloat("Volume", volume);
+    }
+
+    public void SetFullscreen(bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
+        PlayerPrefs.SetInt("Fullscreen", isFullscreen ? 1 : 0);
+    }
 }
