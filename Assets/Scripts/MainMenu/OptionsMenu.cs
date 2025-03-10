7dd15050f19090a2e45ec2039df75981a609a127
Aidan Caughey <caugheya@oregonstate.edu>
2025-03-09 18:00:01 -0700

    Options Menu
    Fixed fullscreen and volume settings

--- Assets/Scripts/MainMenu/OptionsMenu.cs
+++ Assets/Scripts/MainMenu/OptionsMenu.cs
@@ -1,31 +1,20 @@
 using UnityEngine;
 using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Audio;
 
 public class OptionsMenu : MonoBehaviour
 {
-    [SerializeField] private Slider volumeSlider;
-    [SerializeField] private Toggle fullscreenToggle;
-
-    private Resolution[] resolutions;
-
-    private void Start()
-    {
-        // Load saved settings
-        volumeSlider.value = PlayerPrefs.GetFloat("Volume", 1f);
-        fullscreenToggle.isOn = PlayerPrefs.GetInt("Fullscreen", 1) == 1;
-        AudioListener.volume = volumeSlider.value;
-        Screen.fullScreen = fullscreenToggle.isOn;
-    }
+    public AudioMixer audioMixer;
 
     public void SetVolume(float volume)
     {
-        AudioListener.volume = volume;
-        PlayerPrefs.SetFloat("Volume", volume);
+        audioMixer.SetFloat("volume", volume);
     }
 
     public void SetFullscreen(bool isFullscreen)
     {
         Screen.fullScreen = isFullscreen;
-        PlayerPrefs.SetInt("Fullscreen", isFullscreen ? 1 : 0);
     }
 }
